edf8e7dd0b5621d23b61045a78f9fd77e0c63715
vvsatyanarayana <veerasatyanarayana111@gmail.com>
2021-05-18 12:24:08 +0530

    chat

--- src/pages/Bidding/index.js
+++ src/pages/Bidding/index.js
@@ -1,26 +1,111 @@
-
 import React, { useState, useEffect } from "react";
 import io from "socket.io-client";
-// import "./App.css";
+import "./app.css";
 
 let socket;
-const CONNECTION_PORT = "http://localhost:3000/";
+const CONNECTION_PORT = "localhost:3000/";
 
 export function Bidding() {
+  // Before Login
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [room, setRoom] = useState("");
+  const [userName, setUserName] = useState("");
 
-  socket = io(CONNECTION_PORT);
-  socket.on("connection")
+  // After Login
+  const [message, setMessage] = useState("");
+  const [messageList, setMessageList] = useState([]);
 
   useEffect(() => {
-    console.log("ekkeek")
     socket = io(CONNECTION_PORT);
   });
 
+  useEffect(() => {
+    socket.on("receive_message", (data) => {
+      setMessageList([...messageList, data]);
+    });
+  });
+  const connectToRoom = () => {
+    setLoggedIn(true);
+    socket.emit("join_room", room);
+  };
+
+  const sendMessage = async () => {
+    let messageContent = {
+      room: room,
+      content: {
+        author: userName,
+        message: message,
+      },
+    };
+
+    await socket.emit("send_message", messageContent);
+    // setMessageList([...messageList, messageContent.content]);
+    const newlist = messageList.concat(messageContent.content)
+    setMessageList(newlist)
+    console.log(newlist)
+    console.log(messageList)
+    setMessage("");
+  };
+
+  const list = messageList.map((data)=>{
+    return <ul>
+        <li>{data.author}</li>
+        <li>{data.message}</li>
+      </ul>
+  })
   return (
     <div className="App">
-      Bidding
+      {!loggedIn ? (
+        <div className="logIn">
+          <div className="inputs">
+            <input
+              type="text"
+              placeholder="Name..."
+              onChange={(e) => {
+                setUserName(e.target.value);
+              }}
+            />
+            <input
+              type="text"
+              placeholder="Room..."
+              onChange={(e) => {
+                setRoom(e.target.value);
+              }}
+            />
+          </div>
+          <button onClick={connectToRoom}>Enter Chat</button>
+        </div>
+      ) : (
+        <div className="chatContainer">
+          <div className="messages">
+            {messageList.map((val, key) => {
+              return (
+                <div key={key}
+                  className="messageContainer"
+                  id={val.author == userName ? "You" : "Other"}
+                >
+                  <div className="messageIndividual">
+                    {val.author}: {val.message}
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+
+          <div className="messageInputs">
+            <input
+              type="text"
+              placeholder="Message..."
+              onChange={(e) => {
+                setMessage(e.target.value);
+              }}
+            />
+            <button onClick={sendMessage}>Send</button>
+          </div>
+        </div>
+      )}
+      {list}
     </div>
   );
 }
 
-// export default App;
